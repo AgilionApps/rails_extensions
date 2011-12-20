a79005887e0626c4c592942d79ccb5878ac7c26a
Sean Behan <bseanvt@gmail.com>
2011-12-19 19:24:04 -0500

    whitespace... nooooooo

--- lib/rails_extensions/string.rb
+++ lib/rails_extensions/string.rb
@@ -1,16 +1,16 @@
 #
 module RailsExtensions
-	module String
-		def to_sha1
-			Digest::SHA1.hexdigest(self)
-		end
+  module String
+    def to_sha1
+      Digest::SHA1.hexdigest(self)
+    end
 
-		def to_slug(delimiter="-")
-			self.gsub(/[^a-z0-9]/i, " ").strip.gsub(/\s/, '-').gsub(/-{2}/, '-').downcase
-		end
+    def to_slug(delimiter="-")
+      self.gsub(/[^a-z0-9]/i, " ").strip.gsub(/\s/, '-').gsub(/-{2}/, '-').downcase
+    end
 
-	  def to_email
-	    self.scan(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/)
-	  end
-	end
+    def to_email
+      self.scan(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/)
+    end
+  end
 end
--- test/string.rb
+++ test/string.rb
@@ -2,18 +2,18 @@ require 'helper'
 
 class StringTest < Test::Unit::TestCase
 
-	def test_to_sha1
-		assert_equal "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8", "password".to_sha1
-	end
+  def test_to_sha1
+    assert_equal "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8", "password".to_sha1
+  end
 
-	def test_to_slug
-		assert_equal "hello-world", "Hello World".to_slug
-		assert_equal "hello-world-how-are-you", "Hello World, How are You?".to_slug
-	end
+  def test_to_slug
+    assert_equal "hello-world", "Hello World".to_slug
+    assert_equal "hello-world-how-are-you", "Hello World, How are You?".to_slug
+  end
 
-	def test_to_email
-		assert_equal Array, "".to_email.class, "".to_email.class
-  	assert "hello bseanvt@gmail.com how are you?".to_email.include?("bseanvt@gmail.com")
-  	assert "sean@agilionapps.com and adam@agilionapps.com are both email addresses".to_email.include?("adam@agilionapps.com")
-	end
+  def test_to_email
+    assert_equal Array, "".to_email.class, "".to_email.class
+    assert "hello bseanvt@gmail.com how are you?".to_email.include?("bseanvt@gmail.com")
+    assert "sean@agilionapps.com and adam@agilionapps.com are both email addresses".to_email.include?("adam@agilionapps.com")
+  end
 end
